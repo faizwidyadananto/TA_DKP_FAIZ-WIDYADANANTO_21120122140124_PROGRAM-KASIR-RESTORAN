9640afc07c9b0b93bf2b3641a13b207178b4cf4f
faizwidyadananto <faizwidyadananto@gmail.com>
2023-06-04 23:35:22 +0700

    first commit

---
+++ src/new_kasir/New_Kasir.java
@@ -0,0 +1,21 @@
+package new_kasir;
+
+/**
+ *
+ * @author Faiz
+ */
+public class New_Kasir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        login l = new login();
+        l.setVisible(true);
+    }
+
+      
+
+    
+}
